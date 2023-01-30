449603abd66f99bfa4d07b90799ae1903d50c238
suguuss <spam.sugusgaming@gmail.com>
2023-01-30 19:04:49 +0100

    fixed a few things

--- src/main.rs
+++ src/main.rs
@@ -41,20 +41,21 @@ async fn main() {
 		if is_key_down(KeyCode::Q) {update_player_angle(&mut map, -1.0)}
 		if is_key_down(KeyCode::E) {update_player_angle(&mut map, 1.0)}
 
-		let rays = shoot_rays(&map);
+		let walls = shoot_rays(&map);
 
 		clear_background(SKYBLUE);
 		draw_rectangle(0.0, screen_height()/2.0, screen_width(), screen_height()/2.0, BROWN);
+		
+		let rect_size = (screen_width() / walls.len() as f32).ceil();
 
-		for ray in rays.iter().enumerate() 
+		for wall in walls.iter().enumerate() 
 		{
-			let rect_size = (screen_width() / rays.len() as f32).ceil();
+			let wall_nb = wall.0 as f32;
+			let dist_x = (wall.1.0 - map.player.x).abs();
+			let dist_y = (wall.1.1 - map.player.y).abs();
 
-			let ray_nb = ray.0 as f32;
-			let dist_x = (ray.1.0 - map.player.x).abs();
-			let dist_y = (ray.1.1 - map.player.y).abs();
-
-			let angle = ray_nb as f32 / ANGLE_RESOLUTION as f32 - map.player.fov as f32/2.0;
+			// Get the distance between the player and the wall and fix the distortion
+			let angle = wall_nb as f32 / ANGLE_RESOLUTION as f32 - map.player.fov as f32/2.0;
 			let distortion = angle.to_radians().cos().abs();
 			let total_dist = dist_x.hypot(dist_y) * distortion;
 
@@ -62,6 +63,7 @@ async fn main() {
 
 			if wall_size < 50.0 {wall_size = 50.0;}
 
+			// Divide the distance by the max distance to get a value between 0 and 1
 			let wall_color = Color {
 				r: wall_size / (screen_height() * 0.8),
 				g: wall_size / (screen_height() * 0.8),
@@ -69,10 +71,10 @@ async fn main() {
 				a: 255.0,
 			};
 
-			draw_rectangle(ray_nb * rect_size, screen_height()/2.0 - (wall_size/2.0), rect_size as f32, wall_size, wall_color);
+			draw_rectangle(wall_nb * rect_size, screen_height()/2.0 - (wall_size/2.0), rect_size as f32, wall_size, wall_color);
 		}
 		
-		draw_2d_map(&map, &rays);
+		draw_2d_map(&map, &walls);
 
 		next_frame().await
 	}
